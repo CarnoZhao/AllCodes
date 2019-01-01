161cc9162991c8211c61926833eb3e7833d6164c
Carno <273806108@qq.com>
2019-01-01 18:31:54 +0800

    19.01.01

--- R Learning/CMaxrot.R
+++ R Learning/CMaxrot.R
@@ -1,16 +1,28 @@
-rot = function(n){
+lenth = function(n){
 	i = -1
-	while(10 ^ (i + 1) < n){
-		i = i + 1
-	}
-	num1 = n %/% 10 ^ i
-	num2 = n %% 10 ^ i
-	return(num2 * 10 + num1)
+	while(10 ^ (i + 1) < n){i = i + 1}
+	return(i)
+}
+
+rot = function(n, pos){
+	i = lenth(n)
+	newn = n %% 10 ^ (i - pos + 1)
+	pre = n - newn
+	i = i - pos
+	num1 = newn %/% 10 ^ i
+	num2 = newn %% 10 ^ i
+	return(pre + num2 * 10 + num1)
 }
 
 maxRot <- function(n) {
-  # your code
-  
+	i = 0
+	list = c(n)
+	while(i < lenth(n)){
+		n = rot(n, i)
+		list = c(list, n)
+		i = i + 1
+	}
+	return(list)
 }
 
-rot(123)
+maxRot(240522578)
