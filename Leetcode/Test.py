bab7361fd02944c19c388ee634f8268088fefd6c
Carno <273806108@qq.com>
2019-01-04 23:28:06 +0800

    19.01.04.2

--- Leetcode/Test.py
+++ Leetcode/Test.py
@@ -1,18 +1,4 @@
-import time
-from math import factorial
- 
-def factoria(N):
-    N2=N
-    a=list(range(1,N+1))
-    while N2>1:
-        N1=N2%2; N2=N2//2+N1
-        for i in range(N2-N1):
-            a[i]*=a[N2+i]
-        a=a[0:N2]
-    return a[0]
- 
-t=time.clock()
-a=factoria(100000)
-t=time.clock()-t
-print (a%10000000000)
-print("time: %10.5fs" % (t))
+if [2] and [1]:
+	print(1)
+else:
+	print(2)
