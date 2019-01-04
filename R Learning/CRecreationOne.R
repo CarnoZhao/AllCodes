fdcd31a690b6dec1c18d4a61f90e339ec2c705a5
Carno <273806108@qq.com>
2019-01-04 09:21:22 +0800

    19.01.04

--- R Learning/CRecreationOne.R
+++ R Learning/CRecreationOne.R
@@ -1,9 +1,12 @@
 fitFunc = function(x){
-	return(sqrt(sum(c(1:x)[which(x %% c(1:x) == 0)] ^ 2))) ^ 2 == sum(c(1:x)[which(x %% c(1:x) == 0)])))
+	sqdivs = sqrt(sum(c(1:x)[which(x %% c(1:x) == 0)] ^ 2))
+	check = sqdivs == floor(sqdivs)
+	return(check)
 }
 
 listSquared <- function (m, n) {
-	0
+	allfits = c(m:n)[which(sapply(c(m:n), fitFunc))]
+	return(lapply(allfits, function(x){c(x, sum(c(1:x)[which(x %% c(1:x) == 0)] ^ 2))}))
 }
 
-fitFunc(42)
+listSquared(40, 250)
