3e86f8d6b001ba075b3dbdb91feb2b8ff98fee5c
Carno <273806108@qq.com>
2019-01-28 10:49:02 -0500

    19.01.28

--- Leetcode/LMyCalender.py
+++ Leetcode/LMyCalender.py
@@ -1,3 +1,14 @@
+def findMinDiff(temp, startend, S_or_E):
+    minvalue = 2 ** 31 - 1
+    for timepoint, overlap in temp.items():
+        if timepoint > startend:
+            continue
+        value = startend - timepoint
+        if value < minvalue:
+            minvalue = value
+            tempvalue = overlap + 1 if S_or_E == 'start' else max(overlap - 1, 0)
+    return tempvalue
+
 class MyCalendarTwo:
 
     def __init__(self):
@@ -10,18 +21,26 @@ class MyCalendarTwo:
         :rtype: bool
         """
         temp = dict(self.points.items())
-        temp[start] = sorted(filter(lambda x: x[0] <= 0, map(lambda y: (y[0] - start, y[1]), temp.items())), key = lambda z: z[0])[-1][1] + 1
-        temp[end] = sorted(filter(lambda x: x[0] <= 0, map(lambda y: (y[0] - end, y[1]), temp.items())), key = lambda z: z[0])[-1][1]
+        temp[start] = findMinDiff(temp, start, 'start')
         for key in temp:
-                    if start < key < end:
-                        temp[key] += 1
-        if 3 in [x[1] for x in temp.items()]:
+            if start < key <= end:
+                temp[key] += 1
+        temp[end] = findMinDiff(temp, end, 'end')
+        #print(temp)
+        if 3 in temp.values():
             return False
         else:
             self.points = temp
             return True
 
 s = MyCalendarTwo();
-ls = [[],[10,20],[50,60],[10,40],[5,15],[5,10],[25,55]]
+ls = [[],[47,50],[1,10],[27,36],[40,47],[20,27],[15,23],[10,18],[27,36],[17,25],[8,17],[24,33],[23,28],[21,27],[47,50],[14,21],[26,32],[16,21],[2,7],[24,33],[6,13],[44,50],[33,39],[30,36],[6,15],[21,27],[49,50],[38,45],[4,12],[46,50],[13,21]]
 for x in ls[1:]:
-    print(s.book(x[0], x[1]))
+    print(s.book(x[0], x[1]), end = ' ')
+    print(x)
+'''for x in ls[1:]:
+    for i in range(x[0]):
+        print(' ', end = '')
+    for i in range(x[0], x[1]):
+        print('-', end = '')
+    print()'''
