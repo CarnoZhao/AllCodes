20255a59b1fa21981724863ad92d9014aa7eaf0b
Carno <273806108@qq.com>
2019-02-08 21:34:39 -0500

    19.02.08

---
+++ JavaCodes/Test.java
@@ -0,0 +1,10 @@
+public class Test{
+	public static void main(String[] args){
+		double a = 2.2;
+		if (a == 2.2) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
