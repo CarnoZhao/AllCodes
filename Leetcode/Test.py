8ef943ea76850ac8a2ad0dd69a7a9288caea8d41
Carno <273806108@qq.com>
2019-02-24 09:12:13 -0500

    19.02.24

--- Leetcode/Test.py
+++ Leetcode/Test.py
@@ -1,7 +1,1 @@
-import numpy as np
-from itertools import combinations
-energy = lambda x1, x2, x3: 'energy'
-n = 5
-z = np.array([list(x) + [energy(x[0], x[1], x[2])] for x in combinations([i for i in range(1, n + 1) for j in range(3)], 3)])
-for line in z:
-	print('\t'.join(line.tolist()))
+print('Wonderful Git!')
