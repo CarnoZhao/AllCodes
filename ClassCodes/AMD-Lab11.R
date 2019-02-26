2c40d6adf24ad4363004b79c37b3b9fa68e31812
Carno <273806108@qq.com>
2019-02-26 14:39:16 -0500

    19.02.26-14:39:16

--- ClassCodes/AMD-Lab11.R
+++ ClassCodes/AMD-Lab11.R
@@ -9,9 +9,26 @@ library(ggplot2)
 d = data.frame(x, y)
 X1 = d$X1
 X2 = d$X2
-ggplot(d, aes(x = X1, y = X2, color = as.factor(y))) + geom_point()
+graph = ggplot(d, aes(x = X1, y = X2, color = as.factor(y))) + geom_point()
 mf = model.frame(y ~ -1 + X1 + X2, d)
 data = list(x = model.matrix(mf), y = model.response(mf))
-print(data[1])
-print('#######')
-print(data[2])
+
+
+f = function(par, y, x){
+	v = par[-length(par)]
+	c = par[length(par)]
+	ret = -sum(y * log(plogis(x %*% v - c)) + (1 - y) * log(1 - plogis(x %*% v - c)))
+	return(ret)
+}
+
+par = runif(d, -1, 1)
+optimFit = optim(par, f, y = y, x = x)
+par = optimFit$par
+graph = graph + geom_abline(intercept = par[3] / par[2], 
+			slope = -par[1] / par[2], color = 'red')
+graph = graph + geom_abline(intercept = c / v[2], 
+			slope = -v[1] / v[2], color = 'green')
+plot(graph)
+
+glmFit = glm(y ~ X1 + X2, d, family = binomial)
+summary(optimFit)
