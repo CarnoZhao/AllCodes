7ef8babb75642824130666688cb468923deecc1f
Carno <273806108@qq.com>
2019-02-27 09:12:40 -0500

    19.02.27-09:12:39

--- OtherFiles/GitPush.py
+++ OtherFiles/GitPush.py
@@ -3,5 +3,6 @@ import os
 s = time.strftime("%y.%m.%d-%H:%M:%S", time.localtime())
 os.system('d:; cd Codes')
 os.system('git add -A')
+os.system('git status')
 os.system('git commit -m %s' % s)
 os.system('git push')
