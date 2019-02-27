a93f64f64d26b1fd9aa2bfa06684ee99ec3619e2
Carno <273806108@qq.com>
2019-02-27 09:10:19 -0500

    19.02.27-09:10:19

--- Leetcode/LDNF.py
+++ Leetcode/LDNF.py
@@ -1,5 +1,5 @@
 class Solution:
-    def calculateMinimumHP(self, d):
+    def calculateMinimumHP(self, d, rev = False):
         try:
             r, c = len(d), len(d[0])
         except:
@@ -32,9 +32,20 @@ class Solution:
                         if hp2 <= 0 else \
                         (mtx[i - 1][j][0], hp2)
                     mtx[i][j] = tuple1 if tuple1[0] < tuple2[0] else tuple2
+        if not rev:
+            for line in d:
+                print('\t'.join(str(x) for x in line))
+            print()
+            for line in mtx:
+                print('\t'.join(str(x) for x in line))
+        else:
+            for line in mtx[::-1]:
+                print('\t'.join(str(x) for x in line[::-1]))
         return mtx[-1][-1][0]
 
 sol = Solution()
-d = [[1,-3,3],[0,-2,0],[-3,-3,-3]]
+d = [[1,-10, 99],[0,0,0],[-3,-3,0]]
 ans = sol.calculateMinimumHP(d)
+print()
+ans2 = sol.calculateMinimumHP([x[::-1] for x in d[::-1]], True)
 print(ans)
