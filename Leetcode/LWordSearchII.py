0a901d42b812db92953bc0e263835b538297de4a
Carno <273806108@qq.com>
2019-02-27 21:30:27 -0500

    19.02.27-21:30:26

--- Leetcode/LWordSearchII.py
+++ Leetcode/LWordSearchII.py
@@ -1,21 +1,38 @@
 import collections
 class Solution:
     def findWords(self, board, words):
-        dic = collections.defaultdict(list)
         try:
             r, c = len(board), len(board[0])
         except:
             return []
-        for i in range(r):
-            for j in range(c):
-                if i != r - 1:
-                    dic[board[i][j]].append(board[i + 1][j])
-                    dic[board[i + 1][j]].append(board[i][j])
-                if j != c - 1:
-                    dic[board[i][j]].append(board[i][j + 1])
-                    dic[board[i][j + 1]].append(board[i][j])
-        words = {word for word in words if all(word[i + 1] in dic[word[i]] for i in range(len(word) - 1))}
-        print(words)
+        ret = []
+        for word in words:
+            for i in range(r):
+                for j in range(c):
+                    if board[i][j] == word[0] and self.search(word[1:], board, [(i, j)], i, j, r, c):
+                        ret.append(word)
+        return ret
+            
+    def search(self, word, board, used, i, j, r, c):
+        dircs = []
+        if i != 0:
+            dircs.append((i - 1, j))
+        if j != 0:
+            dircs.append((i, j - 1))
+        if i != r - 1:
+            dircs.append((i + 1, j))
+        if j != c - 1:
+            dircs.append((i, j + 1))
+        for x, y in dircs:
+            if board[x][y] == word[0]:
+                if len(word) == 1:
+                    ret = True
+                else:
+                    ret = self.search(word[1:], board, used + [(x, y)], x, y, r, c)
+                if ret:
+                    return True
+        return False
+                
 
 
 sol = Solution()
