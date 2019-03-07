8315299273599d4d324aa27b181a03e22e8472ac
Carno <273806108@qq.com>
2019-03-06 23:01:22 -0500

    19.03.06-23:01:21

--- ClassCodes/Test.R
+++ ClassCodes/Test.R
@@ -1,3 +1,20 @@
-mtx = matrix(c(1:20), 5)
-mu = c(0.1, 0.2, 0.3, 0.4)
-mtx - mu
+library(rethinking)
+d = read.csv('simulated_trees.csv')
+centered.height = d$height - mean(d$height)
+centered.d = data.frame(centered.height, d$age)
+colnames(centered.d) = c('centered.height', 'age')
+ggplot(centered.d, aes(x = centered.height, y = d$age)) + 
+	geom_point() + 
+	xlab('Centered Height') + 
+	ylab('Age') + 
+	ggtitle('Age ~ Centered Height')
+model = map(
+	alist(
+		age ~ dnorm(mu, sigma), 
+		mu <- a + b * height,
+		a ~ dnorm(0, 50),
+		b ~ dnorm(0, 50),
+		sigma ~ dcauchy(0, 5)),
+	start = list(a = 50, b = 0, sigma = 50),
+	data = d)
+precis(model, prob = 0.99)
