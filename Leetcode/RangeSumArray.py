0a96249672a272241535b4e203e3339d2b3c7381
Carno <273806108@qq.com>
2019-03-11 17:34:22 -0400

    19.03.11-17:34:22

--- Leetcode/RangeSumArray.py
+++ Leetcode/RangeSumArray.py
@@ -21,7 +21,7 @@ class Node:
                 else:
                     temp.extend((None, None))
             ls = temp
-        return str(string)
+        return '\n'.join(str(x) for x in string)
 
 class NumArray:
 
@@ -30,12 +30,31 @@ class NumArray:
         self.lenth = len(nums)
 
     def update(self, i: int, val: int) -> None:
-        if i < (len(nums) + 1) // 2:
-            
+        l, r = 0, self.lenth
+        node = self.Tree
+        plus = []
+        while l < r:
+            plus.append(node)
+            mid = (l + r) // 2
+            if i <= mid:
+                r = mid
+                node = node.left
+            else:
+                l = mid
+                node = node.right
+        diff = val - node.val
+        for n in plus + [node]:
+            n.val += diff
 
     def sumRange(self, i: int, j: int) -> int:
-        pass
+        
+
+    def __str__(self):
+        return self.Tree.__str__()
 
 
-node = Node([1, 2, 3, 4, 5])
-print(node)
+import random
+na = NumArray([random.randint(0, 10) for x in range(10)])
+print(na, '\n')
+na.update(0, 2)
+print(na)
