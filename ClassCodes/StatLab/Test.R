0560735c1107123ef554868a9444b4a9689e1789
CarnoZhao <273806108@qq.com>
2019-04-09 12:42:17 -0400

    19.04.09-12.42.13

--- ClassCodes/StatLab/Test.R
+++ ClassCodes/StatLab/Test.R
@@ -5,27 +5,11 @@ data(Kline)
 d = Kline
 d$log_pop = log(d$population)
 str(d)
-graph = ggplot(d, aes(x = log_pop, y = total_tools)) + 
-	geom_point() + 
-	geom_smooth(method = 'lm', se = FALSE, fullrange = TRUE) + 
-	xlim(c(0, 15)) + 
-	ylim(c(0, 80))
-plot(graph)
 model.pois = map(
 	alist(
 		total_tools ~ dpois(lambda),
 		lambda <- exp(a + b * log_pop),
 		a ~ dnorm(0, 100),
-		b ~ dnorm(0, 1),
+		b ~ dnorm(0, 1)
 		),
 	data = d)
-model.gaus = map(
-	alist(
-		total_tools ~ dnorm(mu, sigma),
-		mu <- a + b * log_pop,
-		a ~ dnorm(-50, 10),
-		b ~ dnorm(0, 10),
-		sigma ~ dunif(0, 10)),
-	data = d)
-precis(model.pois, prob = 0.97)
-precis(model.gaus, prob = 0.97)
