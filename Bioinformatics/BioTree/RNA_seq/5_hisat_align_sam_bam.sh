ef4a6e74fa1d99897079d2dcaf2fa4536f16ae76
CarnoZhao <273806108@qq.com>
2019-04-17 12:44:15 -0400

    19.04.17-12.44.15

--- Bioinformatics/BioTree/RNA_seq/5_hisat_align_sam_bam.sh
+++ Bioinformatics/BioTree/RNA_seq/5_hisat_align_sam_bam.sh
@@ -2,6 +2,9 @@ hisat2 -p 8 -x $index -1 $fq1 -2 $fq2 -S $sam
 samtools sort -O bam
 for name in `ls`
 do
-		samtools sort -O bam -@ 5 -o $bam $sam
+		samtools view -b -S $sam > $bam
+		samtools sort -o $sorted_bam $bam
+		samtools index $sorted_bam
+		samtools flagstat $sorted_bam > $bam_flagstat
 done
-samtools index $bam
+
