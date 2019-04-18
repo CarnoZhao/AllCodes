55085bb65bccc9a55adf35d298a7c004dc038101
CarnoZhao <273806108@qq.com>
2019-04-18 19:30:10 -0400

    19.04.18-19.30.10

--- ClassCodes/MiningLab/ADM-Class24.R
+++ ClassCodes/MiningLab/ADM-Class24.R
@@ -1,25 +1,51 @@
 pixels = as.matrix(read.table('../Data/uspsdata.txt'))
 labels = as.matrix(read.table('../Data/uspscl.txt'))
 
+n1 = 256
+n2 = 2
+n3 = 1
+
 sigmoid = function(x){
 		1 / (1 + exp(-x))
 }
 
 weight = function(){
-		w1 = rnorm(256 * 2, 0, 1)
-		dim(w1) = c(256, 2)
-		w2 = rnorm(2, 0, 1)
+		w1 = rnorm((n1 + 1) * n2, 0, 1)
+		dim(w1) = c(n1 + 1, n2)
+		w2 = rnorm((n2 + 1) * n3, 0, 1)
+		dim(w2) = c(n2 + 1, n3)
+		return(list(w1 = w1, w2 = w2))
 }
 
-forward = function(w1, w2){
-		a2 = pixels %*% w1
-		z2 = sigmoid(a2)
+forward = function(pixels, w1, w2){
+		z1 = cbind(rep(1, nrow(pixels)), pixels)
+		a2 = z1 %*% w1
+		z2 = cbind(rep(1, nrow(a2)), sigmoid(a2))
 		a3 = z2 %*% w2
 		z3 = sigmoid(a3)
-		return(list(z2 = z2, z3 = z3))
+		return(list(z1 = z1, z2 = z2, z3 = z3))
 }
 
-backward = function(pixels, z2, z3, labels){
+backward = function(z1, z2, z3, labels, w2){
 		dw2 = t(z2) %*% (labels - z3)
-		dw1 = (labels - z3) %*% t(w2)
+		dw1 = t(z1) %*% ((labels - z3) %*% t(w2) * z2 * (1 - z2))
+		return(list(dw1 = dw1, dw2 = dw2))
 }
+
+main = function(){
+		w1_w2 = weight()
+		w1 = w1_w2$w1
+		w2 = w1_w2$w2
+		for (i in 1:100){
+				result = forward(pixels, w1, w2)
+				print(sum(round(result$z3) != labels))
+				result = backward(result$z1, result$z2, result$z3, labels, w2)
+				print(dim(w1))
+				print(dim(result$dw1))
+				w1 = w1 - result$dw1
+				w2 = w2 - result$dw2
+				print(result$dw2)
+		}
+}
+
+main()
--- Leetcode/Test.py
+++ Leetcode/Test.py
@@ -1,4 +1,4 @@
 from collections import Counter
-with open('LDNF.py', 'r') as f:
+with open('file', 'r') as f:
 	dic = Counter([len(line) for i, line in enumerate(f) if i % 2 == 0])
-print(dic)
+return dic
