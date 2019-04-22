d71b2c9d0a1afb297eda31ed49a4390af200e58f
CarnoZhao <273806108@qq.com>
2019-04-22 14:26:44 -0400

    19.04.22-14.26.44

--- Leetcode/Test.py
+++ Leetcode/Test.py
@@ -1,4 +1,11 @@
-from collections import Counter
-with open('file', 'r') as f:
-	dic = Counter([len(line) for i, line in enumerate(f) if i % 2 == 0])
-return dic
+import random 
+import pandas as pd
+import numpy as np
+
+name = [random.choice('ACGT') for _ in range(100)]
+value = [random.randint(-100, 1000) for _ in range(100)]
+
+df = pd.DataFrame({'name':name, 'value':value})
+print(df['value'].argmax)
+# print(df.groupby('name')['value'].apply(lambda x: max(abs(x)) * (1 if max(abs(x)) in x['value'] else -1)))
+
