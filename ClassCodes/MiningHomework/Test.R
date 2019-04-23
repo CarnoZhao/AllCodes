35277a4d4ddf1679a277dd66354a5aee43aabe99
CarnoZhao <273806108@qq.com>
2019-04-23 09:57:54 -0400

    19.04.23-09.57.53

--- ClassCodes/MiningHomework/Test.R
+++ ClassCodes/MiningHomework/Test.R
@@ -1,77 +1,19 @@
-train_g = function(m, data){
-    x.pos.s = as.matrix(data[data[ncol(data)] == 1, -ncol(data)])
-    x.neg.s = as.matrix(data[data[ncol(data)] != 1, -ncol(data)])
-    pos.index = sample(1:nrow(x.pos.s), m, replace = TRUE)
-    neg.index = sample(1:nrow(x.neg.s), m, replace = TRUE)
-    x.pos.m = x.pos.s[pos.index,]
-    x.neg.m = x.neg.s[neg.index,]
-    if(m == 1){
-        x.pos.m = t(as.matrix(x.pos.m))
-        x.neg.m = t(as.matrix(x.neg.m))
-    }
-    w.m = (x.pos.m - x.neg.m) / rowSums((x.pos.m - x.neg.m) ^ 2)
-    c.w.m = rowSums(w.m * (x.pos.m + x.neg.m) / 2)
-    misc = 
-        rowSums(w.m %*% t(x.pos.s) < c.w.m) + 
-        rowSums(w.m %*% t(x.neg.s) > c.w.m)
-    v = w.m * ifelse(misc > nrow(data) / 2, -1, 1)
-    c = rowSums(v * (x.pos.m + x.neg.m) / 2)
-    return(list(v, c))
+w1 = c(1, 1)
+w2 = c(1, 1 / 2)
+w3 = c(1, -1)
+w4 = c(1, -1 / 2)
+X = seq(-10, 10, length.out = 101)
+sigmoid = function(x) as.matrix(1 / (1 + exp(-x)))
+max0 = function(x) as.matrix(ifelse(x > 0, x, 0))
+f = function(X, W, phi){
+	W = t(as.matrix(W))
+	X = as.matrix(X)
+	W2 = as.matrix(c(1, 1))
+	Z = phi(X %*% W)
+	Y_ = sigmoid(Z %*% W2)
 }
-
-classify = function(x, V, c){
-    return(sign(colSums(sign(V %*% t(x) - c))))
-}
-
-hw = function(){
-    data.d = read.csv('../Data/wdbc.data')
-    data.l = read.csv('../Data/wdbc.labels')
-    data = data.frame(data.d, data.l)
-    data = data[sample(1:nrow(data)),]
-    data.tr = data[1:round(nrow(data) / 2),]
-    data.ts = data[(round(nrow(data) / 2) + 1):nrow(data),]
-    m.seq = seq(1, 199, 2)
-    err.rates = sapply(
-        m.seq, 
-        function(m){
-            Vc = train_g(m, data.tr)
-            V = Vc[[1]]
-            c = Vc[[2]]
-            esti = classify(data.ts[,-ncol(data.ts)], V, c)
-            err.rate = mean(esti != data.ts[,ncol(data.ts)])
-            return(err.rate)
-        }
-    )
-    library(ggplot2)
-    graph = ggplot(data.frame(cbind(m.seq, err.rates)), 
-        aes(x = m.seq, y = err.rates)) + 
-        geom_smooth(formula = y ~ x, method = 'loess') +
-        geom_point() +
-        xlab('m') +
-        ylab('Error Rate')
-    plot(graph)
-}
-
-easyPlot = function(){
-    x1 = c(rnorm(100, 0, 1), rnorm(100, 2, 1))
-    x2 = c(rnorm(100, 0, 1), rnorm(100, 2, 1))
-    x3 = c(rnorm(100, 0, 1), rnorm(100, 2, 1))
-    x4 = c(rnorm(100, 0, 1), rnorm(100, 2, 1))
-    y = c(rep(1, 100), rep(-1, 100))
-    data = data.frame(x1, x2, x3, x4, y)
-    Vc = train_g(100, data)
-    V = Vc[[1]]
-    c = Vc[[2]]
-    esti = classify(data[,-ncol(data)], V, c)
-    newdata = cbind(data, esti)
-    library(ggplot2)
-    shape = c(20, 4)
-    names(shape) = c('TRUE', 'FALSE')
-    graph = ggplot(data, aes(x = x1, y = x2, color = as.factor(esti), shape = as.factor(y == esti))) +
-        geom_point() + 
-        scale_shape_manual(values = shape)
-    print(mean(y != esti))
-    plot(graph)
-}
-
-hw()
+Ws = rbind(w1, w2, w3, w4)
+par(mfrow = c(2, 2))
+apply(Ws, 1, FUN = function(w)plot(X, f(X, w, sigmoid), type = 'l'))
+par(mfrow = c(2, 2))
+apply(Ws, 1, FUN = function(w)plot(X, f(X, w, max0), type = 'l'))
--- Leetcode/Test.py
+++ Leetcode/Test.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 
 name = [random.choice('ACGT') for _ in range(100)]
-value = [random.randint(-100, 1000) for _ in range(100)]
+value = [random.randint(-1000, 1000) for _ in range(100)]
 
-df = pd.DataFrame({'name':name, 'value':value})
-print(df['value'].argmax)
-# print(df.groupby('name')['value'].apply(lambda x: max(abs(x)) * (1 if max(abs(x)) in x['value'] else -1)))
+df = pd.read_table('/mnt/d/Tencent/Files/gene_Psi.txt')
+df.columns = ['name', 'value']
+df.value.apply(eval)
+print(df.groupby('name')['value'].apply(lambda x: max(abs(x)) * (1 if max(abs(x)) in x.tolist() else -1)))
 
